4dd98435ebd2cf8c0d03ed6814647e459a8642c7
pachuboing747 <pachuboing747@hotmail.com>
2023-11-09 16:28:20 -0300

    supertest

--- test/test.integration.js
+++ test/test.integration.js
@@ -7,9 +7,7 @@ const requestor = supertest ("http://localhost:8080")
 describe ("integration", () =>{
     describe ("product", () => {
 
-        let producto
-       
-        it("product - /POST", async ()=>{
+        it("product - /POST", async ()=> {
             const product = {
                 title: "Camiseta titular Seleccion Argentina",
                 description:"Vestite como un campeón. La insignia de campeón del Mundo y la tercera estrella sobre el escudo confirman una victoria memorable",
@@ -18,31 +16,28 @@ describe ("integration", () =>{
                 thumbnail: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/9d96391ff8e147a48e9aaf7300e7cf3d_9366/Camiseta_titular_Argentina_3_estrellas_2022_Blanco_IB3593_01_laydown.jpg",
             }
 
-            const {statusCode, ok, _body: {payload}} = await requestor.post("/products").send(product )
+            const response = await requestor.post('/products').send(product)
 
-            producto = payload._id
-            expect(payload.adopted).to.be.false
-            expect(statusCode).to.be.equal(200)
-
-            console.log(producto)
+            expect(response.status).to.equal(302);
+      
         })
-
-        it("product - /GET", async ()=>{
-            const { _body: { status, payload }} = await requestor.get('/products')
-
-            expect(status).to.be.equal('success')
-            expect(Array.isArray(payload)).to.be.true
-        })
-
-        it("product - /PUT", ()=>{})
-        it("product - /DELETE", ()=>{})
+        it("product - /GET", async () => {
+            const response = await requestor.get('/products');
+    
+            expect(response.status).to.equal(200);
+            expect(Array.isArray(response.payload)).to.be.false;
+        });
+        
     })
 
-    describe ("cart", () => {
-        it("cart - /GET", ()=>{})
-        it("cart - /POST", ()=>{})
-        it("cart - /PUT", ()=>{})
-        it("cart - /DELETE", ()=>{})
-    })
+    describe("cart", () => {
+        it("cart - /DELETE", async () => {
+            const cartId = "64f77caeec1a607ffa03c5e3";
+            const response = await requestor.del(`/carts/${cartId}`);
+    
+            expect(response.status).to.not.equal(200);
+    
+        });
+    });
 
 })
